bb3c904e3ee47825464c926bef7b083208b2bbce
ferp130508 <ferp130508@gmail.com>
2025-03-01 00:00:34 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import numpy as np
 print("="*15 + "Matrix Calculator" + "="*15)
 print(" Pilih Operasi Matrix ")
-print("1.Penjumlahan\n2.Pengurangan\n3.Perkalian\n")
+
+# Input
+operasi = int(input("1.Penjumlahan\n2.Pengurangan\n3.Perkalian\n"))
+
 valuesA = list(map(int, input("Masukkan 4 angka untuk matriks A (dipisah spasi): ").split()))
 valuesB = list(map(int, input("Masukkan 4 angka untuk matriks B (dipisah spasi): ").split()))
  
-# convert
+# Convert 
 matrixA = np.array(valuesA).reshape(2, 2)
 matrixB = np.array(valuesB).reshape(2, 2)
  
 # Operasi
 def penjumlahan(a,b):
-  return  a + b
- 
+    return  a + b
+
+def Pengurangan(a,b):
+    return a - b
+
+def Perkalian(a,b):
+    return a @ b
+
+# Case
+if operasi == 1:
+    matrixC = penjumlahan(matrixA,matrixB)
+    pilihanOperasi = "+"
+elif operasi == 2:
+    matrixC = Pengurangan(matrixA,matrixB)
+    pilihanOperasi = "-"
+elif operasi == 3:
+    matrixC = Perkalian(matrixA,matrixB)
+    pilihanOperasi = "*"
 # Output
-matrixC = penjumlahan(matrixA,matrixB)
 print("Matriks A:\n", matrixA)
 print("Matriks B:\n", matrixB)
-print("Hasil A + B:\n", matrixC)
+print(f"Hasil A {pilihanOperasi} B adalah:\n {matrixC}")
