45908bcac9d08683822fc856970c9d7ac3852d4b
ferp130508 <ferp130508@gmail.com>
2025-03-06 15:26:36 +0000

    modified:   matrix.py

--- matrix.py
+++ matrix.py
@@ -3,23 +3,26 @@ print("="*15 + "Matrix Calculator" + "="*15)
 print(" Pilih Operasi Matrix ")
 
 # Input
-jenisMatrix = int(input("pilih matrix : \n 1.Matrix 2x2\n 2.Matrix 3x3"))
-operasi = int(input("1.Penjumlahan\n2.Pengurangan\n3.Perkalian\n4.Transpose\n5.Determinan\n6.Invers\n"))
+jenisMatrix = int(input("Pilih matrix : \n 1. Matrix 2x2\n 2. Matrix 3x3\n"))
+operasi = int(input("1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Transpose\n5. Determinan\n6. Invers\n"))
 
 if jenisMatrix == 1:
     x, y = 2, 2
 elif jenisMatrix == 2:
     x, y = 3, 3
+else:
+    print("Jenis matrix tidak valid.")
+    exit()
 
 if operasi < 1 or operasi > 6:
     print("Operasi tidak valid. Silakan pilih antara 1 hingga 6.")
 else:
-    valuesA = list(map(int, input(f"Masukkan {x+y} angka untuk matriks A (dipisah spasi): ").split()))
+    valuesA = list(map(int, input(f"Masukkan {x*y} angka untuk matriks A (dipisah spasi): ").split()))
     matrixA = np.array(valuesA).reshape(x, y)
     matrixB = ''
 
     if operasi <= 3:
-        valuesB = list(map(int, input(f"Masukkan {x+y} angka untuk matriks B (dipisah spasi): ").split()))
+        valuesB = list(map(int, input(f"Masukkan {x*y} angka untuk matriks B (dipisah spasi): ").split()))
         matrixB = np.array(valuesB).reshape(x, y)
  
  
@@ -43,13 +46,14 @@ def Invers(a):
     return np.linalg.inv(a)
 
 def printHasil(matrixA, matrixB, matrixC, pilihanOperasi):
-    if operasi <=3 & operasi>0:
-         print(f"Matrix A:\n {matrixA}")
-         print(f"Matrix B:\n {matrixB}")
-         print(f"Hasil Matrix A {pilihanOperasi} Matrix B adalah:\n {matrixC}")
-    elif operasi >= 4 & operasi <= 6:
-         print(f"Matrix A:\n {matrixA}")
-         print(f"Hasil {pilihanOperasi} Matrix A adalah:\n {matrixC}")
+    if matrixB is not None:
+        print(f"Matrix A:\n{matrixA}")
+        print(f"Matrix B:\n{matrixB}")
+        print(f"Hasil Matrix A {pilihanOperasi} Matrix B adalah:\n{matrixC}")
+    else:
+        print(f"Matrix A:\n{matrixA}")
+        print(f"Hasil {pilihanOperasi} Matrix A adalah:\n{matrixC}")
+
 
 # Case
 if operasi == 1:
