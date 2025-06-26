09df7f94bcc63ba88627b35ff398475d65d876a9
Frz013 <ferp130508@gmail.com>
2025-06-26 16:39:52 +0700

    new file:   __pycache__/math_module.cpython-313.pyc 	new file:   __pycache__/matrix.cpython-313.pyc 	new file:   import.py     new file:   math_module.py

---
+++ import.py
@@ -0,0 +1,40 @@
+import math_module as  mtk
+
+print("="*15 + " Selamat datang di kalkulator operasi perhitungan dasar matematika " + "="*15 + "\n")
+print("Silahkan pilih operasi yang di inginkan: ")
+operasi = int(input("1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n5. Pangkat\n6. Faktorial\n7. Modulus\n"))
+
+if operasi == 1:
+    print("="*7 + " Penjumlahan" + "="*7)
+    nilai = list(map(int, input("masukan nilai a+b+c+... dst (hanya angka dan dipisah spasi): ").split()))
+    print(f"Hasilnya adalah: {mtk.tambah(*nilai)}")
+
+elif operasi == 2:
+    print("="*7 + " Pengurangan" + "="*7)
+    nilai = list(map(int, input("masukan nilai a-b-c-... dst (hanya angka dan dipisah spasi): ").split()))
+    print(f"Hasilnya adalah: {mtk.kurang(*nilai)}")
+
+elif operasi == 3:
+    print("="*7 + " Perkalian" + "="*7)
+    nilai = list(map(int, input("masukan nilai a*b*c*... dst (hanya angka dan dipisah spasi): ").split()))
+    print(f"Hasilnya adalah: {mtk.kali(*nilai)}")
+
+elif operasi == 4:
+    print("="*7 + " Pembagian" + "="*7)
+    nilai = list(map(int, input("masukan nilai a/b/c/... dst (hanya angka dan dipisah spasi): ").split()))
+    print(f"Hasilnya adalah: {mtk.bagi(*nilai)}")
+
+elif operasi == 5:
+    print("="*7 + " Pangkat" + "="*7)
+    nilai = list(map(int, input("masukan nilai a pangkat b (hanya angka dan dipisah spasi): ").split()))
+    print(f"Hasilnya adalah: {mtk.pangkat(*nilai)}")
+
+elif operasi == 6:
+    print("="*7 + " Faktorial" + "="*7)
+    nilai = list(map(int, input("masukan nilai n! (hanya angka dan dipisah spasi): ").split()))
+    print(f"Hasilnya adalah: {mtk.faktorial(*nilai)}")
+
+elif operasi == 7:
+    print("="*7 + " Modulus" + "="*7)
+    nilai = list(map(int, input("masukan nilai a mod b (hanya angka dan dipisah spasi): ").split()))
+    print(f"Hasilnya adalah: {mtk.modulus(*nilai)}")
