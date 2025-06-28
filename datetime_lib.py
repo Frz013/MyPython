4020aa25040bdd10a1a8323ed1e515f588fc5a2a
Frz013 <ferp130508@gmail.com>
2025-06-28 15:41:52 +0700

    modified:   datetime_lib.py

--- datetime_lib.py
+++ datetime_lib.py
@@ -1,10 +1,17 @@
-import datetime
+from datetime import datetime
+import time
 
-data_waktu = datetime.datetime.now().date()
-
+data_waktu = datetime.now().date()
+data_jam = datetime.now()
 hari = data_waktu.strftime("%A")
 tanggal = data_waktu.strftime("%d")
 bulan = data_waktu.strftime("%B")
 tahun = data_waktu.strftime("%Y")
 
 print(hari, tanggal, bulan, tahun)
+
+while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("\r" + current_time, end="", flush=True)
+    time.sleep(1)
