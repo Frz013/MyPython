3ccf6f25cf5c0cb842807fc9d6e32e5dd88f298b
Frz013 <ferp130508@gmail.com>
2025-07-13 21:07:51 +0700

    modified:   readWrite.py

--- readWrite.py
+++ readWrite.py
@@ -3,4 +3,10 @@ file = open('text.txt', 'r')
 print(file.readline(), end="")
 print(file.readlines())
 
+file.close
 
+with open("text.txt", "r") as file:
+    isi = file.read()
+    print(isi)
+    print(file.closed)
+print(file.closed)
