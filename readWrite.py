0858960db8ec53d15499718dc7deaa720aed7e0f
Frz013 <ferp130508@gmail.com>
2025-07-14 22:13:47 +0700

    modified:   data.txt 	modified:   readWrite.py

--- readWrite.py
+++ readWrite.py
@@ -14,3 +14,11 @@ print(file.closed)
 with open("data.txt", "w", encoding="utf-8") as file2:
     file2.write("Hello World1\n")
     file2.write("Hello World2\n")
+
+with open("data.txt", "r+", encoding="utf-8") as file2:
+    file2.write("Hello World3\n")
+    file2.write("Hello World4\n")
+
+with open("data.txt", "r+", encoding="utf-8") as file2:
+    data = file2.read()
+    print(data)
