ee98e9e1e83c7b4f14c1433ffbde154a92b1ffe0
Frz013 <ferp130508@gmail.com>
2025-07-15 21:49:02 +0700

    modified:   tryAndExept.py

--- tryAndExept.py
+++ tryAndExept.py
@@ -1,10 +1,23 @@
 # penggunaan try-except dalam Python
 
-try:
-    angka = int(input("Masukkan sebuah angka: "))
-    hasil = 10 / angka
-    print("Hasil pembagian 10 dengan", angka, "adalah", hasil)
-except ValueError:
-    print("Input bukan angka yang valid.")
-except ZeroDivisionError:
-    print("Tidak bisa membagi dengan nol.")
+# try:
+#     angka = int(input("Masukkan sebuah angka: "))
+#     hasil = 10 / angka
+#     print("Hasil pembagian 10 dengan", angka, "adalah", hasil)
+# except ValueError:
+#     print("Input bukan angka yang valid.")
+# except ZeroDivisionError:
+#     print("Tidak bisa membagi dengan nol.")
+
+
+while(True):
+
+    try:
+        angka = int(input("masukan angka: "))
+        hasil = 10/angka
+        print(f"hasil: {hasil}")
+        isLanjut = input("apakah ingin lanjut? (Y/n)")
+        if isLanjut == "n":
+            break
+    except:
+        print("tidak bisa membagi dengan nol")
