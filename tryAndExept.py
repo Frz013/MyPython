6d64542025f49f468cff46bede5486f927c1d00c
Frz013 <ferp130508@gmail.com>
2025-07-17 21:19:32 +0700

    modified:   tryAndExept.py

--- tryAndExept.py
+++ tryAndExept.py
@@ -1,13 +1,13 @@
-# penggunaan try-except dalam Python
+penggunaan try-except dalam Python
 
-# try:
-#     angka = int(input("Masukkan sebuah angka: "))
-#     hasil = 10 / angka
-#     print("Hasil pembagian 10 dengan", angka, "adalah", hasil)
-# except ValueError:
-#     print("Input bukan angka yang valid.")
-# except ZeroDivisionError:
-#     print("Tidak bisa membagi dengan nol.")
+try:
+    angka = int(input("Masukkan sebuah angka: "))
+    hasil = 10 / angka
+    print("Hasil pembagian 10 dengan", angka, "adalah", hasil)
+except ValueError:
+    print("Input bukan angka yang valid.")
+except ZeroDivisionError:
+    print("Tidak bisa membagi dengan nol.")
 
 
 while(True):
