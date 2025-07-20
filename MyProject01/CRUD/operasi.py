b9a922fa18a1b6690b25a25c14cc4e0fb5c10e16
Frz013 <ferp130508@gmail.com>
2025-07-20 14:23:13 +0700

    modified:   MyProject01/CRUD/__pycache__/operasi.cpython-313.pyc 	modified:   MyProject01/CRUD/operasi.py 	new file:   MyProject01/data.txt

--- MyProject01/CRUD/operasi.py
+++ MyProject01/CRUD/operasi.py
@@ -16,7 +16,12 @@ def create_data():
     data["judul"] = judul + database.TEMPLATE["judul"][len(judul):]
     data["tahun"] = tahun
 
-    data_str = f" {data['pk']} | {data['date_add']} | {data['penulis']} | {data['judul']} | {data['tahun']}"
+    data_str = f" {data['pk']} | {data['date_add']} | {data['penulis']} | {data['judul']} | {data['tahun']}\n"
 
+    try:
+        with open(database.DB_FILE, "w", encoding="utf-8") as file:
+            file.write(data_str)
+    except:
+        print("eror")
     print(data_str)
     input("pause")
